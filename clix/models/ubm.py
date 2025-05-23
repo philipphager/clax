c136e1b490035212b0e3455fd0ff60c897afcfc6
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-05-23 12:33:00 +0200

    Update UBM

--- clix/models/ubm.py
+++ clix/models/ubm.py
@@ -5,10 +5,10 @@ import jax.numpy as jnp
 from flax import nnx
 from flax import struct
 from jax import Array
+from jax import lax
 
 from clix.models.loss import binary_cross_entropy
 from clix.models.parameters import BernoulliEmbedding
-from clix.models.utils import last_clicked_positions as _last_clicked_positions
 
 
 @struct.dataclass
@@ -54,8 +54,8 @@ class UserBrowsingModel(nnx.Module):
         clicks = batch["clicks"]
         positions = batch["positions"]
 
-        last_clicked_positions = _last_clicked_positions(positions, clicks)
-        exam_log_probs = self.examination.log_prob(self._examination_param_idx(
+        last_clicked_positions = self._last_clicked_positions(positions, clicks)
+        exam_log_probs = self.examination.log_prob(self._examination_parameters(
             positions,
             last_clicked_positions,
         ))
@@ -80,7 +80,7 @@ class UserBrowsingModel(nnx.Module):
         attraction = mask & jax.random.bernoulli(rngs(), attr_probs)
 
         for idx in range(n_positions):
-            exam_probs = self.examination.prob(self._examination_param_idx(
+            exam_probs = self.examination.prob(self._examination_parameters(
                 positions[:, idx],
                 last_clicked_positions,
             ))
@@ -100,6 +100,18 @@ class UserBrowsingModel(nnx.Module):
             attraction=attraction,
         )
 
-    def _examination_param_idx(self, positions, last_clicked_positions):
+    def _examination_parameters(self, positions, last_clicked_positions):
         examination_idx = positions * self.positions + last_clicked_positions
         return {"examination_idx": examination_idx}
+
+    @staticmethod
+    def _last_clicked_positions(positions: Array, clicks: Array) -> Array:
+        # Filter clicked positions, e.g.: [1, 2, 3, 4], [1, 0, 0, 1] -> [1, 0, 0, 4]
+        clicked_positions = jnp.where(clicks == 1, positions, 0)
+        # Find the last clicked position for each item: [1, 0, 0, 4] -> [1, 1, 1, 4]
+        # Assumes positions are sorted in ascending order!
+        clicked_positions = lax.cummax(clicked_positions, axis=1)
+        # Shift the clicked positions to the right to align with the next item:
+        clicked_positions = jnp.roll(clicked_positions, shift=1, axis=1)
+        # Set the first position to 0, as there is no previously clicked position:
+        return clicked_positions.at[:, 0].set(0)

f1d598f6b5e439fa40cd8269b04ba1c78e1f36f9
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-05-29 13:58:35 +0200

    Update loss.py

--- clix/models/loss.py
+++ clix/models/loss.py
@@ -1,6 +1,7 @@
 import jax.numpy as jnp
-
 from jax import Array
+
+from clix.models.math import log1mexp
 
 
 def binary_cross_entropy(
@@ -8,7 +9,7 @@ def binary_cross_entropy(
 ):
     if log_probs:
         p_click = y_predict
-        p_no_click = jnp.log1p(-jnp.exp(y_predict))
+        p_no_click = log1mexp(y_predict)
     else:
         p_click = jnp.log(y_predict)
         p_no_click = jnp.log1p(-1.0 * y_predict)

aecccb889f413a4558b527f20bb993e41014ec21
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-06-02 10:09:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,53 @@
+from pathlib import Path
+
 import optax
 import torch
 from flax import nnx
 from torch.utils.data import DataLoader
 
-from clix.datasets import YandexDataset
 from clix.models import (
     PositionBasedModel,
 )
 from clix.trainer import Trainer
+from clix.yandex import YandexDataset
 
 
 def main():
     rngs = nnx.Rngs(0)
-    dataset = YandexDataset("data/yandex.csv")
-    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [0.6, 0.4])
-    val_dataset, test_dataset = torch.utils.data.random_split(val_dataset, [0.5, 0.5])
+    import jax
+
+    print(jax.devices())
+
+    path = Path("/ivi/ilps/datasets/yandex/relevance_prediction/YandexClicks.txt")
+    index_path = Path("data/wscd-2012/index.json")
+
+    train_dataset = YandexDataset(path, index_path, session_range=(0, 10_000_000))
+    val_dataset = YandexDataset(
+        path, index_path, session_range=(10_000_000, 15_000_000)
+    )
+    test_dataset = YandexDataset(
+        path, index_path, session_range=(15_000_000, 20_000_000)
+    )
 
     train_loader = DataLoader(
         train_dataset,
         batch_size=512,
-        collate_fn=dataset.collate_fn,
+        num_workers=8,
+        collate_fn=train_dataset.collate_fn,
     )
     val_loader = DataLoader(
         val_dataset,
         batch_size=512,
-        collate_fn=dataset.collate_fn,
+        num_workers=4,
+        collate_fn=val_dataset.collate_fn,
     )
     test_loader = DataLoader(
         test_dataset,
         batch_size=512,
-        collate_fn=dataset.collate_fn,
+        num_workers=4,
+        collate_fn=test_dataset.collate_fn,
     )
-    model = PositionBasedModel(rngs=rngs, query_doc_pairs=140_000, positions=10)
+    model = PositionBasedModel(rngs=rngs, query_doc_pairs=120_000_000, positions=10)
     trainer = Trainer(optax.adam(1e-3), epochs=10, patience=0)
     train_df = trainer.train(model, train_loader, val_loader)
     test_df = trainer.test(model, test_loader)

c8bddca3866e71719c071287d396a58db210dbc9
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-06-02 10:18:42 +0200

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+#SBATCH --time=1:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64GB
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+
+source ${HOME}/.bashrc
+mamba activate two-tower-confounding
+
+python main.py

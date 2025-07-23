e080ca71e15803ecb0a9ca3ffd805c939b0a6d23
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-23 21:17:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,35 @@
 from pathlib import Path
 from time import perf_counter
 
+import hydra
 import optax
-import pandas as pd
 from flax import nnx
+from hydra.utils import instantiate
+from omegaconf import DictConfig, OmegaConf
 from torch.utils.data import DataLoader
-from jax.lib import xla_bridge
 
 from clix.datasets.yandex import YandexDataset
-from clix.models import (
-    GlobalCTRModel,
-    DocumentBasedCTRModel,
-    RankBasedCTRModel,
-    UserBrowsingModel,
-    DynamicBayesianNetwork,
-    ClickChainModel,
-    DependentClickModel,
-)
-from clix.models.pbm import PositionBasedModel
+from clix.models import PositionBasedModel, UserBrowsingModel
+from clix.parameters import EmbeddingParameterConfig, HashEmbedding
 from clix.trainer import Trainer
 
-
-def get_judged_queries(path: Path):
-    """
-    Data Format Source:
-    https://web.archive.org/web/20130828200420/http://imat-relpred.yandex.ru/en/datasets
-    """
-    test_df = pd.read_csv(
-        path,
-        sep="\t",
-        names=["query_id", "region_id", "query_doc_ids", "label"],
-        usecols=["query_id"],
-    )
-    return set(test_df["query_id"].unique().tolist())
+OmegaConf.register_new_resolver("eval", eval)
 
 
-def main():
-    print(xla_bridge.get_backend().platform)
+@hydra.main(version_base="1.3", config_path="clix/config/", config_name="config")
+def main(config: DictConfig):
+    print(OmegaConf.to_yaml(config))
+    rngs = nnx.Rngs(config.random_state)
 
-    rngs = nnx.Rngs(0)
-    path = Path("/ivi/ilps/datasets/yandex/relevance_prediction/YandexClicks.txt")
+    path = Path("data/wscd-2012/YandexClicks.txt")
     index_path = Path("data/wscd-2012/index.json")
 
-    train_dataset = YandexDataset(path, index_path, session_range=(0, 6_000_000))
-    val_dataset = YandexDataset(path, index_path, session_range=(6_000_000, 8_000_000))
+    train_dataset = YandexDataset(path, index_path, session_range=(0, 10_000_000))
+    val_dataset = YandexDataset(
+        path, index_path, session_range=(10_000_000, 15_000_000)
+    )
     test_dataset = YandexDataset(
-        path, index_path, session_range=(8_000_000, 10_000_000)
+        path, index_path, session_range=(15_000_000, 20_000_000)
     )
 
     train_loader = DataLoader(
@@ -70,55 +54,19 @@ def main():
         collate_fn=test_dataset.collate_fn,
     )
 
-    query_doc_pairs = 10_000_000
-    positions = 10
+    model_fn = instantiate(config.model)
+    model = model_fn(rngs=rngs)
 
-    models = [
-        # GlobalCTRModel(rngs=rngs),
-        # DocumentBasedCTRModel(rngs=rngs, query_doc_pairs=query_doc_pairs),
-        # RankBasedCTRModel(rngs=rngs, positions=positions),
-        # PositionBasedModel(
-        #     rngs=rngs,
-        #     query_doc_pairs=query_doc_pairs,
-        #     positions=positions,
-        # ),
-        UserBrowsingModel(
-            rngs=rngs,
-            query_doc_pairs=query_doc_pairs,
-            positions=positions,
-        ),
-        # DynamicBayesianNetwork(
-        #     rngs=rngs,
-        #     query_doc_pairs=query_doc_pairs,
-        # ),
-        # ClickChainModel(
-        #     rngs=rngs,
-        #     query_doc_pairs=query_doc_pairs,
-        # ),
-        # DependentClickModel(
-        #     rngs=rngs,
-        #     query_doc_pairs=query_doc_pairs,
-        #     positions=positions,
-        # ),
-    ]
+    trainer = Trainer(optax.adamw(1e-3), epochs=50, patience=0)
 
-    train_dfs = []
-    test_dfs = []
+    timer_start = perf_counter()
+    train_df = trainer.train(model, train_loader, val_loader)
+    timer_stop = perf_counter()
+    train_df["train_time_s"] = timer_stop - timer_start
+    test_df = trainer.test(model, test_loader)
 
-    for model in models:
-        trainer = Trainer(optax.adamw(1e-3), epochs=50, patience=0)
-
-        timer_start = perf_counter()
-        train_df = trainer.train(model, train_loader, val_loader)
-        timer_stop = perf_counter()
-        train_df["train_time_s"] = timer_stop - timer_start
-        train_dfs.append(train_df)
-
-        test_df = trainer.test(model, test_loader)
-        test_dfs.append(test_df)
-
-        pd.concat(train_dfs).to_csv("train.csv")
-        pd.concat(test_dfs).to_csv("test.csv")
+    train_df.to_csv("train.csv")
+    test_df.to_csv("test.csv")
 
 
 if __name__ == "__main__":

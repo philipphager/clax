6d478696b7267b787ba77f610416cab3c0a9ae2b
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-23 10:35:55 +0200

    Update deep.py

--- clix/parameters/deep.py
+++ clix/parameters/deep.py
@@ -4,26 +4,54 @@ from flax import nnx
 from flax.nnx import rnglib
 from jax import Array
 
-from clix.models.utils import ActivationFactory
-from clix.parameters.base import Parameter
+
+class ActivationFactory:
+    """
+    Global factory to retrieve activation functions by name.
+    Can be used to register custom activation functions:
+
+    >>> ActivationFactory.register("selu", nnx.selu)
+    """
+
+    name2activation = {
+        "tanh": nnx.tanh,
+        "relu": nnx.relu,
+        "elu": nnx.elu,
+        "gelu": nnx.gelu,
+    }
+
+    @classmethod
+    def get(cls, name):
+        return cls.name2activation[name]
+
+    @classmethod
+    def register(cls, name, activation_fn):
+        if name in cls.name2activation:
+            raise Exception(f"Activation {name} already registered")
+        cls.name2activation[name] = activation_fn
 
 
-class DeepParameter(Parameter):
+class DeepParameter(nnx.Module):
+    """
+    Parameter using input features and a deep feed forward network, e.g.,
+    to model user attraction from query-document features.
+    """
+
     def __init__(
         self,
         use_feature: str,
         features: int,
-        hidden_units: int,
-        layers: int,
-        dropout: float,
-        activation: str,
+        hidden_units: int = 16,
+        layers: int = 2,
+        dropout: float = 0.0,
+        activation: str = "elu",
         *,
         rngs: rnglib.Rngs,
     ):
         super().__init__()
         self.use_feature = use_feature
         modules = []
-        activation_fn = ActivationFactory[activation]
+        activation_fn = ActivationFactory.get(activation)
 
         for _ in range(layers):
             modules.extend(
@@ -39,7 +67,7 @@ class DeepParameter(Parameter):
         self.model = nnx.Sequential(*modules)
 
     def logit(self, batch: Dict) -> Array:
-        return self.model(batch[self.use_feature])
+        return self.model(batch[self.use_feature]).squeeze()
 
     def prob(self, batch: Dict) -> Array:
         return nnx.sigmoid(self.logit(batch))

aed46679f785aa0eb5df9837abba73980eb879b8
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-29 15:16:35 +0200

    Update baidu.py

--- clax/datasets/baidu.py
+++ clax/datasets/baidu.py
@@ -50,16 +50,15 @@ class BaiduULTRDataset(IterableDataset):
 
         for file, begin_row, end_row in file_ranges:
             n_rows = end_row - begin_row
-            df = pl.read_parquet(file).slice(begin_row, n_rows)
+            df = pl.scan_parquet(file).slice(begin_row, n_rows)
 
-            for query_doc_ids, clicks in zip(
-                df["query_doc_ids"].to_numpy(), df["clicks"].to_numpy()
-            ):
-                n = min(len(query_doc_ids), self.max_positions)
+            for row in df.iter_rows(named=True):
+
+                n = min(len(row["query_doc_ids"]), self.max_positions)
 
                 yield {
-                    "query_doc_ids": query_doc_ids[:n],
-                    "clicks": clicks[:n],
+                    "query_doc_ids": row["query_doc_ids"][:n],
+                    "clicks": row["clicks"][:n],
                     "mask": self.mask[:n],
                     "positions": self.positions[:n],
                     "n": n,

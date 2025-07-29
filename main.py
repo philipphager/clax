df7c75d562e41ea4cfba60f4e7707d407cfd127e
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-29 12:53:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main(config: DictConfig):
         train_dataset,
         batch_size=config.train_batch_size,
         collate_fn=train_dataset.collate_fn,
-        num_workers=8,
+        num_workers=14,
         pin_memory=True,
         persistent_workers=True,
         multiprocessing_context=ctx,

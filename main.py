39530d001b5749ff8f8e19830d1208273accd2b0
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-29 15:38:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main(config: DictConfig):
         num_workers=4,
         # prefetch_factor=16,
         # persistent_workers=True,
-        # multiprocessing_context=ctx,
+        multiprocessing_context=ctx,
     )
     val_loader = DataLoader(
         val_dataset,

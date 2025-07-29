1d3bfe129859c3a4847015e722f4ab9049a2389a
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-29 15:32:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,9 +40,9 @@ def main(config: DictConfig):
         train_dataset,
         batch_size=config.train_batch_size,
         collate_fn=train_dataset.collate_fn,
-        num_workers=8,
-        prefetch_factor=16,
-        persistent_workers=True,
+        # num_workers=8,
+        # prefetch_factor=16,
+        # persistent_workers=True,
         multiprocessing_context=ctx,
     )
     val_loader = DataLoader(

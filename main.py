0df37d256dc7819d40bf6d818ba0fb24b433e198
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-29 14:30:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -40,16 +40,19 @@ def main(config: DictConfig):
         train_dataset,
         batch_size=config.train_batch_size,
         collate_fn=train_dataset.collate_fn,
+        num_workers=4,
     )
     val_loader = DataLoader(
         val_dataset,
         batch_size=config.eval_batch_size,
         collate_fn=val_dataset.collate_fn,
+        num_workers=4,
     )
     test_loader = DataLoader(
         test_dataset,
         batch_size=config.eval_batch_size,
         collate_fn=test_dataset.collate_fn,
+        num_workers=4,
     )
 
     model_fn = instantiate(config.model)

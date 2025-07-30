f355113d7aa45cc50b20f504803bcc5dcff72a2f
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-30 08:34:39 +0200

    Update baidu.py

--- clax/datasets/baidu.py
+++ clax/datasets/baidu.py
@@ -1,9 +1,9 @@
-import itertools
 from pathlib import Path
 from typing import List, Tuple, Union
 
 import numpy as np
 import polars as pl
+import pyarrow.parquet as pq
 import torch
 from torch.utils.data import IterableDataset
 
@@ -53,26 +53,24 @@ class BaiduULTRDataset(IterableDataset):
         batched_file_ranges = batched(file_ranges, self.file_batch_size)
 
         for file_ranges in batched_file_ranges:
-            # Create a list of lazy frames
-            lazy_dfs = []
+            dfs = []
+
             for file, begin_row, end_row in file_ranges:
                 n_rows = end_row - begin_row
-                lazy_df = pl.scan_parquet(file).slice(begin_row, n_rows)
-                lazy_dfs.append(lazy_df)
+                df = pl.scan_parquet(file).slice(begin_row, n_rows)
+                dfs.append(df)
 
-            # Use a single lazy plan for the entire batch of files
-            lazy_combined_df = pl.concat(lazy_dfs, rechunk=False)
+            df = pl.concat(dfs).collect()
 
-            # Collect and iterate over the data
-            df_iterator = lazy_combined_df.select(
-                pl.col("query_doc_ids"), pl.col("clicks")
-            ).iter_rows()
-
-            for query_doc_ids, clicks in df_iterator:
+            for query_doc_ids, clicks in zip(
+                df["query_doc_ids"].to_numpy(),
+                df["clicks"].to_numpy(),
+            ):
                 n = min(len(query_doc_ids), self.max_positions)
+
                 yield {
-                    "query_doc_ids": np.array(query_doc_ids[:n], dtype=np.int32),
-                    "clicks": np.array(clicks[:n], dtype=np.float16),
+                    "query_doc_ids": query_doc_ids[:n],
+                    "clicks": clicks[:n],
                     "mask": self.mask[:n],
                     "positions": self.positions[:n],
                     "n": n,

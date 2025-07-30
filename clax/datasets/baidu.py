6b7cb973f20a6514c9ef5ebf94ca679f316b9a36
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-30 09:27:54 +0200

    Update baidu.py

--- clax/datasets/baidu.py
+++ clax/datasets/baidu.py
@@ -3,11 +3,12 @@ from typing import List, Tuple, Union
 
 import numpy as np
 import polars as pl
+import pyarrow.dataset as ds
 import pyarrow.parquet as pq
 import torch
 from torch.utils.data import IterableDataset
 
-from clax.datasets.utils import SessionCollator, batched
+from .utils import SessionCollator, batched
 
 FileRangeTuple = Tuple[Path, int, int]
 
@@ -18,13 +19,9 @@ class BaiduULTRDataset(IterableDataset):
         path: Union[Path, str],
         session_range: Tuple[int, int],
         max_positions: int = 10,
-        file_batch_size: int = 5,
     ):
-        path = Path(path)
-        files = self._find_files(path)
-        self.file_ranges = self._file_ranges(files, session_range)
-        self.max_positions = max_positions
-        self.file_batch_size = file_batch_size
+        self.session_range = session_range
+        self.path = Path(path)
         self.collate_fn = SessionCollator(
             query_features={
                 "n": np.int16,
@@ -37,96 +34,28 @@ class BaiduULTRDataset(IterableDataset):
             },
         )
         # Pre-compute reusable outputs:
+        self.max_positions = max_positions
         self.mask = np.ones(self.max_positions, dtype=np.bool_)
         self.positions = np.arange(1, self.max_positions + 1, dtype=np.int16)
 
     def __len__(self) -> int:
-        total_sessions = 0
-
-        for _, begin_row, end_row in self.file_ranges:
-            total_sessions += end_row - begin_row
-
-        return total_sessions
+        return self.session_range[1] - self.session_range[0]
 
     def __iter__(self):
-        file_ranges = self._get_local_file_ranges()
-        batched_file_ranges = batched(file_ranges, self.file_batch_size)
+        for file in Path(self.path).glob("*.parquet"):
+            file = pq.ParquetFile(file)
 
-        for file_ranges in batched_file_ranges:
-            dfs = []
+            for batch in file.iter_batches():
+                query_doc_ids = batch["query_doc_ids"].to_numpy(zero_copy_only=False)
+                clicks = batch["clicks"].to_numpy(zero_copy_only=False)
 
-            for file, begin_row, end_row in file_ranges:
-                n_rows = end_row - begin_row
-                df = pl.scan_parquet(file).slice(begin_row, n_rows)
-                dfs.append(df)
+                for i in range(len(query_doc_ids)):
+                    n = min(self.max_positions, len(query_doc_ids[i]))
 
-            df = pl.concat(dfs).collect()
-
-            for query_doc_ids, clicks in zip(
-                df["query_doc_ids"].to_numpy(),
-                df["clicks"].to_numpy(),
-            ):
-                n = min(len(query_doc_ids), self.max_positions)
-
-                yield {
-                    "query_doc_ids": query_doc_ids[:n],
-                    "clicks": clicks[:n],
-                    "mask": self.mask[:n],
-                    "positions": self.positions[:n],
-                    "n": n,
-                }
-
-    def _get_local_file_ranges(self) -> List[FileRangeTuple]:
-        """
-        Select a subset of file ranges to iterate, based on the current worker process.
-        See: https://pytorch.org/docs/stable/data.html#torch.utils.data.IterableDataset
-        """
-        info = torch.utils.data.get_worker_info()
-
-        if info is None:
-            workers = 1
-            worker_id = 0
-        else:
-            workers = info.num_workers
-            worker_id = info.id
-
-        return [f for i, f in enumerate(self.file_ranges) if i % workers == worker_id]
-
-    @staticmethod
-    def _file_ranges(
-        files: List[Path],
-        session_range: Tuple[int, int],
-    ) -> List[FileRangeTuple]:
-        """
-        Determine which files should be read (and which range in each file)
-        for a given range of sessions.
-        """
-        file_ranges: List[FileRangeTuple] = []
-        session_begin, session_end = session_range
-        total_sessions = 0
-
-        for file in sorted(files):
-            df = pl.scan_parquet(file)
-            num_sessions = df.select(pl.len()).collect().item()
-
-            file_begin = total_sessions
-            file_end = total_sessions + num_sessions
-
-            overlap_begin = max(file_begin, session_begin)
-            overlap_end = min(file_end, session_end)
-
-            if overlap_begin < overlap_end:
-                start_row = overlap_begin - total_sessions
-                end_row = overlap_end - total_sessions
-                file_ranges.append((file, start_row, end_row))
-
-            if total_sessions >= session_end:
-                break
-
-            total_sessions += num_sessions
-
-        return file_ranges
-
-    @staticmethod
-    def _find_files(path: Path) -> List[Path]:
-        return list(path.glob("part-*.parquet"))
+                    yield {
+                        "query_doc_ids": query_doc_ids[i][:n],
+                        "clicks": clicks[i][:n],
+                        "mask": self.mask[:n],
+                        "positions": self.positions[:n],
+                        "n": n,
+                    }

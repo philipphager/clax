dcd178f43e5ae449e92d292f6432c72a7e7766d6
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-07-31 15:57:07 +0200

    Update 1_yandex_10m.sh

--- scripts/1_yandex_10m.sh
+++ scripts/1_yandex_10m.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python main.py -m \
-  experiment=1-yandex-10m \
+  experiment=1-yandex-10m/1/ \
   model='glob(*)' \
   dataset=yandex \
   train_sessions=[0,10_000_000] \
@@ -10,5 +10,31 @@ python main.py -m \
   eval_train_queries_only=True \
   parameter/attraction=embedding/full \
   parameter/satisfaction=embedding/full \
-  random_state=2025 \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=1-yandex-10m/2/ \
+  model='glob(*)' \
+  dataset=yandex \
+  train_sessions=[10_000_000,20_000_000] \
+  val_sessions=[20_000_000,25_000_000] \
+  test_sessions=[25_000_000,30_000_000] \
+  eval_train_queries_only=True \
+  parameter/attraction=embedding/full \
+  parameter/satisfaction=embedding/full \
+  random_state=2 \
+  $@
+
+python main.py -m \
+  experiment=1-yandex-10m/3/ \
+  model='glob(*)' \
+  dataset=yandex \
+  train_sessions=[20_000_000,30_000_000] \
+  val_sessions=[30_000_000,35_000_000] \
+  test_sessions=[35_000_000,40_000_000] \
+  eval_train_queries_only=True \
+  parameter/attraction=embedding/full \
+  parameter/satisfaction=embedding/full \
+  random_state=3 \
   $@

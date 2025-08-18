23a8d88138e2edceac995e7ee1f7d0859e1681d6
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-18 15:12:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def main(config: DictConfig):
     model_fn = instantiate(config.model)
     model = model_fn(rngs=rngs)
 
-    trainer = Trainer(optax.adamw(0.01), epochs=50, patience=0)
+    trainer = Trainer(optax.adamw(0.005), epochs=50, patience=0)
 
     timer_start = perf_counter()
     train_df = trainer.train(model, train_loader, val_loader)

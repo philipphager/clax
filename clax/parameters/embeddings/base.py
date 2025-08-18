47c1663d7c728f8dab33b687bf1c13b036a0c873
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-18 15:17:40 +0200

    WIP disable 0 prior

--- clax/parameters/embeddings/base.py
+++ clax/parameters/embeddings/base.py
@@ -48,7 +48,7 @@ class EmbeddingParameter(Parameter):
         self.embeddings = config.embedding_fn(
             num_embeddings=num_embeddings,
             features=config.embedding_features,
-            embedding_init=config.embedding_init,
+            # embedding_init=config.embedding_init,
             rngs=rngs,
         )
 

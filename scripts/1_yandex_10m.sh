eb42aa8d6c24fdf4daa379d9c4674e9dac622b15
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-18 14:06:20 +0200

    Update 1_yandex_10m.sh

--- scripts/1_yandex_10m.sh
+++ scripts/1_yandex_10m.sh
@@ -4,7 +4,6 @@ python main.py -m \
   experiment=1-yandex-10m/1/ \
   model='glob(*)' \
   dataset=yandex \
-  query_doc_pairs=61_600_000 \
   train_sessions=[0,10_000_000] \
   val_sessions=[10_000_000,15_000_000] \
   test_sessions=[15_000_000,20_000_000] \
@@ -18,7 +17,6 @@ python main.py -m \
   experiment=1-yandex-10m/2/ \
   model='glob(*)' \
   dataset=yandex \
-  query_doc_pairs=61_600_000 \
   train_sessions=[10_000_000,20_000_000] \
   val_sessions=[20_000_000,25_000_000] \
   test_sessions=[25_000_000,30_000_000] \
@@ -32,7 +30,6 @@ python main.py -m \
   experiment=1-yandex-10m/3/ \
   model='glob(*)' \
   dataset=yandex \
-  query_doc_pairs=61_600_000 \
   train_sessions=[20_000_000,30_000_000] \
   val_sessions=[30_000_000,35_000_000] \
   test_sessions=[35_000_000,40_000_000] \

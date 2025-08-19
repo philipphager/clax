f0f9acd9420645eecf2c4ef41fccd99b522d2131
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-19 12:50:21 +0200

    Update 1_yandex_10m.sh

--- scripts/1_yandex_10m.sh
+++ scripts/1_yandex_10m.sh
@@ -4,37 +4,37 @@ python main.py -m \
   experiment=1-yandex-10m/1/ \
   model='glob(*)' \
   dataset=yandex \
-  train_sessions=[0,1_000_000] \
-  val_sessions=[1_000_000,2_000_000] \
-  test_sessions=[2_000_000,3_000_000] \
+  train_sessions=[0,10_000_000] \
+  val_sessions=[10_000_000,15_000_000] \
+  test_sessions=[15_000_000,20_000_000] \
   min_train_sessions_per_eval_query=10 \
   parameter/attraction=embedding/full \
   parameter/satisfaction=embedding/full \
   random_state=1 \
   $@
-#
-#python main.py -m \
-#  experiment=1-yandex-10m/2/ \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  train_sessions=[10_000_000,20_000_000] \
-#  val_sessions=[20_000_000,25_000_000] \
-#  test_sessions=[25_000_000,30_000_000] \
-#  min_train_sessions_per_eval_query=10 \
-#  parameter/attraction=embedding/full \
-#  parameter/satisfaction=embedding/full \
-#  random_state=2 \
-#  $@
-#
-#python main.py -m \
-#  experiment=1-yandex-10m/3/ \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  train_sessions=[20_000_000,30_000_000] \
-#  val_sessions=[30_000_000,35_000_000] \
-#  test_sessions=[35_000_000,40_000_000] \
-#  min_train_sessions_per_eval_query=10 \
-#  parameter/attraction=embedding/full \
-#  parameter/satisfaction=embedding/full \
-#  random_state=3 \
-#  $@
+
+python main.py -m \
+  experiment=1-yandex-10m/2/ \
+  model='glob(*)' \
+  dataset=yandex \
+  train_sessions=[10_000_000,20_000_000] \
+  val_sessions=[20_000_000,25_000_000] \
+  test_sessions=[25_000_000,30_000_000] \
+  min_train_sessions_per_eval_query=10 \
+  parameter/attraction=embedding/full \
+  parameter/satisfaction=embedding/full \
+  random_state=2 \
+  $@
+
+python main.py -m \
+  experiment=1-yandex-10m/3/ \
+  model='glob(*)' \
+  dataset=yandex \
+  train_sessions=[20_000_000,30_000_000] \
+  val_sessions=[30_000_000,35_000_000] \
+  test_sessions=[35_000_000,40_000_000] \
+  min_train_sessions_per_eval_query=10 \
+  parameter/attraction=embedding/full \
+  parameter/satisfaction=embedding/full \
+  random_state=3 \
+  $@

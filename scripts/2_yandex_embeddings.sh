1f08f242449061aa2ef5f379615f340c385fd17f
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-21 12:01:09 +0200

    Update 2_yandex_embeddings.sh

--- scripts/2_yandex_embeddings.sh
+++ scripts/2_yandex_embeddings.sh
@@ -1,21 +1,70 @@
 #!/usr/bin/env bash
+#
+#python main.py -m \
+#  experiment=2-yandex-embeddings/full/ \
+#  model='glob(*)' \
+#  dataset=yandex \
+#  parameter/attraction=embedding/full \
+#  parameter/satisfaction=embedding/full \
+#  random_state=1 \
+#  $@
 
 python main.py -m \
-  experiment=2-yandex-embeddings/hash/ \
+  experiment=2-yandex-embeddings/hash/10 \
   model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/hash \
   parameter/satisfaction=embedding/hash \
-  compression_ratio=10,100,1_000 \
+  compression_ratio=10 \
   random_state=1 \
   $@
 
 python main.py -m \
-  experiment=2-yandex-embeddings/qr/ \
+  experiment=2-yandex-embeddings/hash/100 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=100 \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/1000 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=1000 \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/qr/10 \
   model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
-  compression_ratio=10,100,1_000 \
+  compression_ratio=10 \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/qr/100 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/qr \
+  parameter/satisfaction=embedding/qr \
+  compression_ratio=100 \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/qr/1_000 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/qr \
+  parameter/satisfaction=embedding/qr \
+  compression_ratio=1_000 \
   random_state=1 \
   $@

5b85dbf3e60c41af5da61cb88fbec0df8c6e1b03
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-23 12:40:31 +0200

    Update 2_yandex_embeddings.sh

--- scripts/2_yandex_embeddings.sh
+++ scripts/2_yandex_embeddings.sh
@@ -17,229 +17,229 @@
 #  test_sessions=[90_000_000,100_000_000] \
 #  random_state=1 \
 #  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/full/2 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/full \
-#  parameter/satisfaction=embedding/full \
-#  train_sessions=[20_000_000,100_000_000] \
-#  val_sessions=[100_000_000,110_000_000] \
-#  test_sessions=[110_000_000,120_000_000] \
-#  random_state=2 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/full/3 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/full \
-#  parameter/satisfaction=embedding/full \
-#  train_sessions=[40_000_000,120_000_000] \
-#  val_sessions=[120_000_000,130_000_000] \
-#  test_sessions=[130_000_000,140_000_000] \
-#  random_state=3 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/2/1 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=2 \
-#  train_sessions=[0,80_000_000] \
-#  val_sessions=[80_000_000,90_000_000] \
-#  test_sessions=[90_000_000,100_000_000] \
-#  random_state=1 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/2/2 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=2 \
-#  train_sessions=[20_000_000,100_000_000] \
-#  val_sessions=[100_000_000,110_000_000] \
-#  test_sessions=[110_000_000,120_000_000] \
-#  random_state=2 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/2/3 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=2 \
-#  train_sessions=[40_000_000,120_000_000] \
-#  val_sessions=[120_000_000,130_000_000] \
-#  test_sessions=[130_000_000,140_000_000] \
-#  random_state=3 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/5/1 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=5 \
-#  train_sessions=[0,80_000_000] \
-#  val_sessions=[80_000_000,90_000_000] \
-#  test_sessions=[90_000_000,100_000_000] \
-#  random_state=1 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/5/2 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=5 \
-#  train_sessions=[20_000_000,100_000_000] \
-#  val_sessions=[100_000_000,110_000_000] \
-#  test_sessions=[110_000_000,120_000_000] \
-#  random_state=2 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/5/3 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=5 \
-#  train_sessions=[40_000_000,120_000_000] \
-#  val_sessions=[120_000_000,130_000_000] \
-#  test_sessions=[130_000_000,140_000_000] \
-#  random_state=3 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/10/1 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=10 \
-#  train_sessions=[0,80_000_000] \
-#  val_sessions=[80_000_000,90_000_000] \
-#  test_sessions=[90_000_000,100_000_000] \
-#  random_state=1 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/10/2 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=10 \
-#  train_sessions=[20_000_000,100_000_000] \
-#  val_sessions=[100_000_000,110_000_000] \
-#  test_sessions=[110_000_000,120_000_000] \
-#  random_state=2 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/10/3 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=10 \
-#  train_sessions=[40_000_000,120_000_000] \
-#  val_sessions=[120_000_000,130_000_000] \
-#  test_sessions=[130_000_000,140_000_000] \
-#  random_state=3 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/100/1 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=100 \
-#  train_sessions=[0,80_000_000] \
-#  val_sessions=[80_000_000,90_000_000] \
-#  test_sessions=[90_000_000,100_000_000] \
-#  random_state=1 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/100/2 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=100 \
-#  train_sessions=[20_000_000,100_000_000] \
-#  val_sessions=[100_000_000,110_000_000] \
-#  test_sessions=[110_000_000,120_000_000] \
-#  random_state=2 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/100/3 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=100 \
-#  train_sessions=[40_000_000,120_000_000] \
-#  val_sessions=[120_000_000,130_000_000] \
-#  test_sessions=[130_000_000,140_000_000] \
-#  random_state=3 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/1000/1 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=1000 \
-#  train_sessions=[0,80_000_000] \
-#  val_sessions=[80_000_000,90_000_000] \
-#  test_sessions=[90_000_000,100_000_000] \
-#  random_state=1 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/1000/2 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=1000 \
-#  train_sessions=[20_000_000,100_000_000] \
-#  val_sessions=[100_000_000,110_000_000] \
-#  test_sessions=[110_000_000,120_000_000] \
-#  random_state=2 \
-#  $@
-#
-#python main.py -m \
-#  experiment=2-yandex-embeddings/hash/1000/3 \
-#  model='glob(*)' \
-#  dataset=yandex \
-#  parameter/attraction=embedding/hash \
-#  parameter/satisfaction=embedding/hash \
-#  compression_ratio=1000 \
-#  train_sessions=[40_000_000,120_000_000] \
-#  val_sessions=[120_000_000,130_000_000] \
-#  test_sessions=[130_000_000,140_000_000] \
-#  random_state=3 \
-#  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/full/2 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/full \
+  parameter/satisfaction=embedding/full \
+  train_sessions=[20_000_000,100_000_000] \
+  val_sessions=[100_000_000,110_000_000] \
+  test_sessions=[110_000_000,120_000_000] \
+  random_state=2 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/full/3 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/full \
+  parameter/satisfaction=embedding/full \
+  train_sessions=[40_000_000,120_000_000] \
+  val_sessions=[120_000_000,130_000_000] \
+  test_sessions=[130_000_000,140_000_000] \
+  random_state=3 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/2/1 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=2 \
+  train_sessions=[0,80_000_000] \
+  val_sessions=[80_000_000,90_000_000] \
+  test_sessions=[90_000_000,100_000_000] \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/2/2 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=2 \
+  train_sessions=[20_000_000,100_000_000] \
+  val_sessions=[100_000_000,110_000_000] \
+  test_sessions=[110_000_000,120_000_000] \
+  random_state=2 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/2/3 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=2 \
+  train_sessions=[40_000_000,120_000_000] \
+  val_sessions=[120_000_000,130_000_000] \
+  test_sessions=[130_000_000,140_000_000] \
+  random_state=3 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/5/1 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=5 \
+  train_sessions=[0,80_000_000] \
+  val_sessions=[80_000_000,90_000_000] \
+  test_sessions=[90_000_000,100_000_000] \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/5/2 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=5 \
+  train_sessions=[20_000_000,100_000_000] \
+  val_sessions=[100_000_000,110_000_000] \
+  test_sessions=[110_000_000,120_000_000] \
+  random_state=2 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/5/3 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=5 \
+  train_sessions=[40_000_000,120_000_000] \
+  val_sessions=[120_000_000,130_000_000] \
+  test_sessions=[130_000_000,140_000_000] \
+  random_state=3 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/10/1 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=10 \
+  train_sessions=[0,80_000_000] \
+  val_sessions=[80_000_000,90_000_000] \
+  test_sessions=[90_000_000,100_000_000] \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/10/2 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=10 \
+  train_sessions=[20_000_000,100_000_000] \
+  val_sessions=[100_000_000,110_000_000] \
+  test_sessions=[110_000_000,120_000_000] \
+  random_state=2 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/10/3 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=10 \
+  train_sessions=[40_000_000,120_000_000] \
+  val_sessions=[120_000_000,130_000_000] \
+  test_sessions=[130_000_000,140_000_000] \
+  random_state=3 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/100/1 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=100 \
+  train_sessions=[0,80_000_000] \
+  val_sessions=[80_000_000,90_000_000] \
+  test_sessions=[90_000_000,100_000_000] \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/100/2 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=100 \
+  train_sessions=[20_000_000,100_000_000] \
+  val_sessions=[100_000_000,110_000_000] \
+  test_sessions=[110_000_000,120_000_000] \
+  random_state=2 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/100/3 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=100 \
+  train_sessions=[40_000_000,120_000_000] \
+  val_sessions=[120_000_000,130_000_000] \
+  test_sessions=[130_000_000,140_000_000] \
+  random_state=3 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/1000/1 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=1000 \
+  train_sessions=[0,80_000_000] \
+  val_sessions=[80_000_000,90_000_000] \
+  test_sessions=[90_000_000,100_000_000] \
+  random_state=1 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/1000/2 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=1000 \
+  train_sessions=[20_000_000,100_000_000] \
+  val_sessions=[100_000_000,110_000_000] \
+  test_sessions=[110_000_000,120_000_000] \
+  random_state=2 \
+  $@
+
+python main.py -m \
+  experiment=2-yandex-embeddings/hash/1000/3 \
+  model='glob(*)' \
+  dataset=yandex \
+  parameter/attraction=embedding/hash \
+  parameter/satisfaction=embedding/hash \
+  compression_ratio=1000 \
+  train_sessions=[40_000_000,120_000_000] \
+  val_sessions=[120_000_000,130_000_000] \
+  test_sessions=[130_000_000,140_000_000] \
+  random_state=3 \
+  $@
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/2/1 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -252,7 +252,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/2/2 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -265,7 +265,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/2/3 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -278,7 +278,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/5/1 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -291,7 +291,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/5/2 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -304,7 +304,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/5/3 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -317,7 +317,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/10/1 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -330,7 +330,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/10/2 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -343,7 +343,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/10/3 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -356,7 +356,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/100/1 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -369,7 +369,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/100/2 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -382,7 +382,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/100/3 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -395,7 +395,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/1000/1 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -408,7 +408,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/1000/2 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \
@@ -421,7 +421,7 @@ python main.py -m \
 
 python main.py -m \
   experiment=2-yandex-embeddings/qr/1000/3 \
-  model=drctr \
+  model='glob(*)' \
   dataset=yandex \
   parameter/attraction=embedding/qr \
   parameter/satisfaction=embedding/qr \

faf7e16a59d2d65151e399746f2e72b6a3b91e35
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-26 13:41:43 +0200

    Add input norm to linear param

--- clax/parameters/linear.py
+++ clax/parameters/linear.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, Optional, Callable
 
 from flax import nnx
 from flax.nnx import rnglib
@@ -12,6 +12,7 @@ from clax.parameters.base import Parameter, ParameterConfig
 class LinearParameterConfig(ParameterConfig):
     use_feature: str
     features: int
+    input_norm: Optional[Callable] = nnx.LayerNorm
 
 
 class LinearParameter(Parameter):
@@ -23,6 +24,11 @@ class LinearParameter(Parameter):
     ):
         super().__init__()
         self.config = config
+        self.input_norm = (
+            config.input_norm(config.features, rngs=rngs)
+            if config.input_norm is not None
+            else lambda x: x
+        )
         self.linear = nnx.Linear(
             in_features=config.features,
             out_features=1,
@@ -30,7 +36,8 @@ class LinearParameter(Parameter):
         )
 
     def logit(self, batch: Dict) -> Array:
-        return self.linear(batch[self.config.use_feature]).squeeze()
+        x = self.input_norm(batch[self.config.use_feature])
+        return self.linear(x).squeeze()
 
     def prob(self, batch: Dict) -> Array:
         return nnx.sigmoid(self.logit(batch))

657daadfaf87f61ab5d5513ab177414d0a69574b
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-26 14:43:07 +0200

    Update 4_baidu_ultr_features.sh

--- scripts/4_baidu_ultr_features.sh
+++ scripts/4_baidu_ultr_features.sh
@@ -10,8 +10,8 @@ python main.py -m \
   experiment=4-baidu-ultr-features/1/ \
   model='glob(*)' \
   dataset=baidu-ultr-uva \
-  parameter/attraction=embedding/deep-cross \
-  parameter/satisfaction=embedding/deep-cross \
+  parameter/attraction=deep-cross \
+  parameter/satisfaction=deep-cross \
   train_sessions=[0,1_000_000] \
   val_sessions=[1_000_000,1_200_000] \
   test_sessions=[1_200_000,1_400_000] \
@@ -22,8 +22,8 @@ python main.py -m \
   experiment=4-baidu-ultr-features/2/ \
   model='glob(*)' \
   dataset=baidu-ultr-uva \
-  parameter/attraction=embedding/deep-cross \
-  parameter/satisfaction=embedding/deep-cross \
+  parameter/attraction=deep-cross \
+  parameter/satisfaction=deep-cross \
   train_sessions=[400_000,1_400_000] \
   val_sessions=[1_400_000,1_600_000] \
   test_sessions=[1_600_000,1_800_000] \
@@ -34,8 +34,8 @@ python main.py -m \
   experiment=4-baidu-ultr-features/3/ \
   model='glob(*)' \
   dataset=baidu-ultr-uva \
-  parameter/attraction=embedding/deep-cross \
-  parameter/satisfaction=embedding/deep-cross \
+  parameter/attraction=deep-cross \
+  parameter/satisfaction=deep-cross \
   train_sessions=[800_000,1_800_000] \
   val_sessions=[1_800_000,2_000_000] \
   test_sessions=[2_000_000,2_200_000] \
